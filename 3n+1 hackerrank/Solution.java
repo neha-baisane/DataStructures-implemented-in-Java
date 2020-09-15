2439e157bb21a770822114af1d89ad50f9d4ea10
neha-baisane <64725427+neha-baisane@users.noreply.github.com>
2020-09-15 21:03:49 +0530

    Add files via upload

---
+++ 3n+1 hackerrank/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     int count=1;
+     while(n!=1){
+         if(n%2==0){
+         n=n/2;
+         count++;
+     }else{
+         n=(n*3)+1;
+         count++;
+     }
+         
+     };
+        
+    System.out.println(count);
+    
+    }
+}
